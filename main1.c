e67aa59f68ec1c6b2f8109b37c6f1b90d8b60fc5
Mihir02 <63883198+Mihir02@users.noreply.github.com>
2021-10-31 21:10:19 +0530

    Add files via upload

---
+++ main1.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	/* create temp arrays */
+	int L[n1], R[n2];
+
+	/* Copy data to temp arrays L[] and R[] */
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+
+	/* Merge the temp arrays back into arr[l..r]*/
+	i = 0; // Initial index of first subarray
+	j = 0; // Initial index of second subarray
+	k = l; // Initial index of merged subarray
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	/* Copy the remaining elements of L[], if there
+	are any */
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	/* Copy the remaining elements of R[], if there
+	are any */
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+/* l is for left index and r is right index of the
+sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+	if (l < r) {
+		// Same as (l+r)/2, but avoids overflow for
+		// large l and h
+		int m = l + (r - l) / 2;
+
+		// Sort first and second halves
+		mergeSort(arr, l, m);
+		mergeSort(arr, m + 1, r);
+
+		merge(arr, l, m, r);
+	}
+}
+
+/* UTILITY FUNCTIONS */
+/* Function to print an array */
+void printArray(int A[], int size, int flag)
+{
+	int i;
+  printf("%d ",A[0]);
+	if(flag)
+  {
+    for (i = 1; i < size; i++)
+      if(A[i] != A[i-1])
+		    printf("%d ", A[i]);
+  }
+  else
+    for (i = 1; i < size; i++)
+      printf("%d ", A[i]);
+	printf("\n");
+}
+
+/* Driver code */
+int main()
+{
+  int m,n;
+  printf("Enter the size of 1st set:");
+  scanf("%d",&m);
+  printf("Enter the size of 2nd set:");
+  scanf("%d",&n);
+  int arr_size = m+n;
+	int arr[arr_size];
+  printf("Enter the set1:");
+  for(int i=0;i<m;i++){
+      scanf("%d",&arr[i]);
+  }
+  printf("Enter the set2:");
+  for(int i=m;i<m+n;i++){
+      scanf("%d",&arr[i]);    
+  }
+
+	printf("Given array is \n");
+	printArray(arr, arr_size, 0);
+
+	mergeSort(arr, 0, arr_size - 1);
+
+	printf("\nSorted array is \n");
+	printArray(arr, arr_size, 1);
+	return 0;
+}
